5c485cd805d9878b799d240cc7718ad0e664ca91
karthigeyapandian <karthigeyapandian.raju@emc.com>
2019-01-31 13:05:00 -0700

    securitycheck

---
+++ Run.bat
@@ -0,0 +1,4 @@
+dotnet add Applications/AllocationsServer package Steeltoe.Security.Authentication.CloudFoundryCore --version 2.1.1
+dotnet add Applications/BacklogServer package Steeltoe.Security.Authentication.CloudFoundryCore --version 2.1.1
+dotnet add Applications/RegistrationServer package Steeltoe.Security.Authentication.CloudFoundryCore --version 2.1.1
+dotnet add Applications/TimesheetsServer package Steeltoe.Security.Authentication.CloudFoundryCore --version 2.1.1
